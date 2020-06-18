a065f05e5487445ca85aa251ac02ee67483f8413
Miranda <mirandazhou0830@gmail.com>
2020-06-18 18:09:07 +0800

    Reform the navigation system. Functional change.

--- src/js/app.js
+++ src/js/app.js
@@ -7,13 +7,36 @@ window.jQuery = $;
 
 $(document).ready(
     function () {
-        $('.slide').slick({
-            dots: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 2,
-            adaptiveHeight: true,
-            centerMode: true,
+        if ($('div').hasClass('slide')) {
+            $('.slide').slick({
+                dots: true,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                responsive: [
+                    {
+                        breakpoint: 800,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                        },
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+                    },
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                ],
+            });
+        }
+        $(navbar).fadeIn('slow', function () {
+            $('.content').fadeIn('slow');
         });
     }
 );
